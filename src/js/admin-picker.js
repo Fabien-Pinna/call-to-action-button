6059982383fe57a6c6e218b77c935a1bd4711ba6
Fabien-Pinna <fabien.pinna@gmail.com>
2023-09-18 16:55:17 +0200

    styles added

--- src/js/admin-picker.js
+++ src/js/admin-picker.js
@@ -5,26 +5,36 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const iconPicker = document.getElementById('icon-picker');
     const hiddenInput = document.getElementById('button_icon');
 
-    console.log(iconList);
-
     iconList.forEach((iconObj) => {
+        const iconBox = document.createElement('div');
         const iconElement = document.createElement('i');
+        const iconName = document.createElement('span');
+
+        iconBox.className = 'icon-box'
         iconElement.className = iconObj.icon;
+        iconName.className = 'icon-name';
+        iconElement.title = iconObj.icon.replace('fa fa-', '');
+
+
+
         iconElement.addEventListener('click', function () {
             iconList.forEach((i) => {
                 const existingElement = iconPicker.getElementsByClassName(i.icon)[0];
                 if (existingElement) {
-                    existingElement.classList.remove('selected');
+                    const parentBox = existingElement.parentNode;
+                    parentBox.classList.remove('selected');
                 }
             });
-            this.classList.add('selected');
-            hiddenInput.value = this.className;
+            iconBox.classList.add('selected');
+            hiddenInput.value = iconObj.icon;
         });
 
         if (hiddenInput.value === iconObj.icon) {
-            iconElement.classList.add('selected');
+            iconBox.classList.add('selected');
         }
 
-        iconPicker.appendChild(iconElement);
+        iconBox.appendChild(iconElement);
+        iconBox.appendChild(iconName);
+        iconPicker.appendChild(iconBox);
     });
 });
