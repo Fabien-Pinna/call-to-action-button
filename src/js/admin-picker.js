964ab704b1206b7f2dedf6e2bfa42c8d510afe56
Fabien-Pinna <fabien.pinna@gmail.com>
2023-09-19 12:02:59 +0200

    Some adjustments made on js

--- src/js/admin-picker.js
+++ src/js/admin-picker.js
@@ -5,17 +5,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const iconPicker = document.getElementById('icon-picker');
     const hiddenInput = document.getElementById('button_icon');
 
+    // Vérifier si iconPicker et hiddenInput existent
+    if (!iconPicker || !hiddenInput) {
+        return; // Quitter la fonction si l'un des éléments n'existe pas
+    }
+
     iconList.forEach((iconObj) => {
         const iconBox = document.createElement('div');
         const iconElement = document.createElement('i');
         const iconName = document.createElement('span');
 
-        iconBox.className = 'icon-box'
+        iconBox.className = 'icon-box';
         iconElement.className = iconObj.icon;
         iconName.className = 'icon-name';
         iconElement.title = iconObj.icon.replace('fa fa-', '');
-
-
 
         iconElement.addEventListener('click', function () {
             iconList.forEach((i) => {
@@ -34,7 +37,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
 
         iconBox.appendChild(iconElement);
-        // iconBox.appendChild(iconName);
         iconPicker.appendChild(iconBox);
     });
 });
