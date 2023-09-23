0661b77ce01d9943320fdb67e9eb81c2068bcde1
Fabien-Pinna <fabien.pinna@gmail.com>
2023-09-23 18:45:28 +0200

    Styles on admin code editor done for now

--- src/js/custom-codemirror.js
+++ src/js/custom-codemirror.js
@@ -1,34 +1,36 @@
 import '../scss/custom-codemirror.scss';
 
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM Loaded');
+document.addEventListener('DOMContentLoaded', () => {
     if (typeof wp === "undefined" || typeof wp.codeEditor === "undefined") {
-        console.log("wp.codeEditor is not defined.");
         return;
     }
-    console.log('wp.codeEditor is defined.');
 
-    var textArea = document.getElementById('custom-css');
+    const textArea = document.getElementById('custom-css');
     if (!textArea) {
-        console.log('textArea not found');
         return;
     }
-    console.log('textArea found');
 
-    var editorSettings = window.editorSettings || {};
-    console.log('Editor Settings:', editorSettings);
+    const editorSettings = window.editorSettings || {};
 
     editorSettings.codemirror = Object.assign({}, editorSettings.codemirror, {
         mode: 'css',
         lineNumbers: true,
         indentUnit: 4,
         tabSize: 4,
-        lineWrapping: true
+        lineWrapping: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        autoCloseBrackets: true,
+        autoCloseTags: true,
+        showTrailingSpace: true,
+        extraKeys: {
+            "Ctrl-Space": "autocomplete",
+            "Ctrl-S": "save",
+            "Ctrl-F": "findPersistent"
+
+        }
     });
 
-    console.log('Updated Editor Settings:', editorSettings);
+    const editor = wp.codeEditor.initialize(textArea, editorSettings);
 
-    var editor = wp.codeEditor.initialize(textArea, editorSettings);
-    console.log('Editor initialized:', editor);
 });
-console.log('custom-codemirror.js loaded')
